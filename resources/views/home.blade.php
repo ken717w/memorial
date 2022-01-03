22118c6a83472d05a19299a56ab0c0b2d92e55ba
Ken Wong <ken717w@gmail.com>
2022-01-03 21:30:23 +0800

    Remove personal data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 
         <meta property="og:title" content="{{ config('app.name') }}">
         <meta property="og:description" content="">
-        <meta property="og:image" content="https://i.imgur.com/2defaud.jpg">
+        <meta property="og:image" content="">
 
         {{-- Fonts --}}
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -29,66 +29,21 @@
                     </div>
                 </div>
 
-                <h1 class="display-4 mb-0 font-weight-bold text-center">Cheung Kwai Fan, Helen<br>周張桂芬</h1>
-                <h4 class="mb-0 text-center">24/10/1954 - 17/5/2020</h4>
+                <h1 class="display-4 mb-0 font-weight-bold text-center">Name</h1>
+                <h4 class="mb-0 text-center">Date</h4>
             </div>
         </div>
 
         <div id="section-notice">
-            <div class="container">
-                <img class="img-fluid mb-5" src="https://i.imgur.com/XoDka34.jpg">
-                <img class="img-fluid mb-5" src="https://i.imgur.com/eWLRtwS.jpg">
-            </div>
         </div>
 
         <div id="section-messages">
             <div class="container">
                 <blockquote class="blockquote">
-                    <p>蒙福的才德婦人　周張桂芬女士</p>
+                    <p>Message</p>
 
-                    <p>多謝大家來到一起和我們一起懷緬　媽媽的一生。</p>
-
-                    <p>媽媽很年輕便結婚，當別人還在享受青春時、她已經負起照顧我和妺妹的責任。由於爸爸是警察，上班時間不定時，家中的一切事務也要她處理。媽媽常說，家中的小事由她作主，大事就由爸爸作主，但家中很少會有大事，即使有都會化為小事，在她的管理下，爸爸可以“無後顧之憂”專心工作。媽媽有七兄弟姊妹，姨媽婆婆在加拿大，所以媽媽也擔任家中大家姐甚至媽媽的角色。媽媽很疼錫阿姨和舅父，小時候，每當媽媽帶我們外出玩耍，都一定會帶亞姨和表弟妹同行，一車不下十數人去公園、郊外、沙灘，因此大家關係十分密切。
-                    媽媽除了關心家人之外，也樂於幫助其他有需要的人。小時候媽媽車我們返學，也會車埋鄰居的子女。在宿舍我們本來是住一個三房單位，因為鄰居有需要，她亦願意搬到一個小的單位住。她更在保良局助養了兩個小孩，還記得小時候和她們通信和去保良局探她們。到我們上了中學，她有多些自由時間，便去做義工，去陪診、送飯、到醫院探訪，幫助了許多有需要的人。</p>
-
-                    <p>媽媽雖然讀書不多，但結婚後仍十分好學，家中有許多書藉，當中我印象最深刻的是讀者文摘，見到媽媽每一期也看得津津樂道，我也嘗試去看，但只看得懂頭兩頁笑話那部份。媽媽亦有去學羽毛球、去廚藝學院學烹飪等，得來的不單是課堂上的知識，還有一班一班的好朋友。</p>
-
-                    <p>到了玫希出世，爸媽每天也會由北角長途跋涉駕車到馬鞍山照顧孫兒。當我們放工回家，為了使我們有多些私人時間，她和爸爸也會識趣返北角才吃晚飯，但往往因為太夜，所以只好草草吃飯盒便算。或許由於那樣頻撲又吃得不好，所以那段時間也瘦了許多。她更用Helen婆婆的身份參加了Baby Kingdom群組，原來起初組員對婆婆入組都有保留，但媽媽專程到她們相約的地點扮偶遇，組員才被媽媽的誠意打動，讓她加入，後來也成了好朋友。</p>
-
-                    <p>2012年，媽媽患上了癌症，五姨知道她患病，馬上決定辭工全時間來照顧她，其他亞姨亦不間斷地輪流來陪伴她。媽媽每次入院做手術，莫牧師和何牧師也會專程來醫院到安慰她、為她祈禱，實在十分感謝你們。媽媽那顆幫助人熱心並沒有因患病改變，每次到醫院化療，她總會與病友滔滔不絕地交談和鼓勵身邊的人，為她們祈禱，向她們傳福音。雖然媽媽因為病的原故未能常常返教會，對聖經的認識也不算太深，但她總愛說：「天父說，只要我們兩個人一起祈禱，天父必會誰聽我們的禱告。」患病後有人介紹她吃憂頓草，她也積極將相關的食療介紹給其他病友。有一次在等看醫生時提起憂頓草，更即時叫姑丈把朋友給她的優頓草派給剛認識的病友們，還引起一番騷動。</p>
-
-                    <p>今年三月，媽媽因為肺積水要入院做手術，手術後由於傷口痛，呼吸也有困難，去廁所也要人扶。有一次，她好辛苦去完廁所，以為她會第一時間返病床休息，誰知道她竟走到對面床和一位新入院的病人交談，媽媽說她隱約聽到佢同醫生討論病況，相信她應該患了舞蹈病，她以前在讀者文摘曾看過這病，因為她剛才聽到她在哭，所以特意去安慰她、為她祈禱。病友也因為媽媽的誠意感動到流淚。</p>
-
-                    <p>這是我的媽媽，總是不忘燃點自己的生命去照亮別人。媽媽，當你離開的時候，我們的確十分不捨。不過想到你可以不需再受苦，可以如願以償回天家和爸爸見面，也感到安慰。你常說自己是天父最愛的女兒，沒錯，你真是㡳天父賜。但我想告訴妳，妳亦是我們最賜的媽媽、婆婆、家姐、姨母、三嬸和三叔婆。知道我們只是暫時的分開，他日在天家再重聚。</p>
-
-                    <footer class="blockquote-footer">長女　周芷瑩</footer>
+                    <footer class="blockquote-footer">Author</footer>
                 </blockquote>
-
-                <div class="row">
-                    <div class="col-lg-6">
-                        <blockquote class="blockquote">
-                            <p>姨媽係張家靈魂人物，好照顧，好愛錫屋企每一位。姨媽係我童年時嘅一位重要女主角，我好記得細個時成日都跟住姨媽，佢經常帶我去玩，仲教識我游水，踩roller，打羽毛球，跟住姨媽好開心，佢比咗好多兒時嘅歡樂時光我。到我大個咗姨媽知我考到車牌，佢同我講，要多啲操吓手車，叫我隨時攞佢架車去揸吓，點知有次我撞爛咗架車,但係佢同姨丈都無怪責過我，諗返起呢件事，真係想再同佢哋講聲sorry。姨媽係我好尊敬嘅長輩，所以到我結婚時，我第一時間就諗要搵佢幫我上頭，好記得嗰日佢拎咗好多嘢嚟為我張羅，連床鋪都幫我換埋，又搵佢朋友幫我化妝set頭，出心出力。我真係好幸運有個咁好嘅姨媽，佢和藹可親嘅笑容，親切關懷嘅聲音，我永遠都唔會忘記。姨媽，好多謝你，我愛你！</p>
-
-                            <footer class="blockquote-footer">姨甥女　Nicky 嘉碧</footer>
-                        </blockquote>
-                    </div>
-
-                    <div class="col-lg-6">
-                        <blockquote class="blockquote">
-                            <p>『您是我的守護天使』，這是在座每一位的心聲！</p>
-
-                            <p>近日我想寫點東西懷緬我這位親愛的姐姐，但寫了這兩句就沒法寫下去了。二家姐對我們的愛護實在是太多太多，我們對她的回憶滿滿，實難以筆墨形容！</p>
-
-                            <p>小時候我被狗咬傷，是她抱著我，陪著我看醫生；安安兒時住院，是她擁著我，陪我一起哭，一起心痛；安安托兒在保姆家，是她陪我一起去，還記得那天，她哭著緊緊的抱著安安，不捨得放下他的樣子。事後我倆回憶時都笑說，那時我們一副放低安安就不接回他的樣子，那姨姨當時會不會好驚！</p>
-
-                            <p>太多太多溫馨的回憶足以令我懷念半生，我感恩我生命有您，正如詩歌唱出：全因為您，令我一生佳美！多謝您，我愛您！我永遠懷念您！</p>
-
-                            <p>近日心靈極需要慰寂，時常聽敬拜歌曲，發現有好多首歌曲都讓我想起您，所以我斷定您就是神派給我的使者！感謝主！家姐，如今您的使命已經超額完成，返回天家與哥哥重聚，記望他日我們在天家再聚。</p>
-
-                            <footer class="blockquote-footer">七妹</footer>
-                        </blockquote>
-                    </div>
-                </div>
             </div>
         </div>
 
@@ -101,28 +56,28 @@
                                 <div id="carousel-gallery" class="carousel slide h-100" data-ride="carousel">
                                     <div class="carousel-inner h-100">
                                         <div class="carousel-item h-100 active">
-                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://i.imgur.com/6JhjERc.jpg">
+                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://via.placeholder.com/800x600?text=Sample+Image+1">
                                         </div>
                                         <div class="carousel-item h-100">
-                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://i.imgur.com/vsMPB7M.jpg">
+                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://via.placeholder.com/800x600?text=Sample+Image+2">
                                         </div>
                                         <div class="carousel-item h-100">
-                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://i.imgur.com/9Fs94na.jpg">
+                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://via.placeholder.com/800x600?text=Sample+Image+3">
                                         </div>
                                         <div class="carousel-item h-100">
-                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://i.imgur.com/3G0TYJo.jpg">
+                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://via.placeholder.com/800x600?text=Sample+Image+4">
                                         </div>
                                         <div class="carousel-item h-100">
-                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://i.imgur.com/k8ycfKB.jpg">
+                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://via.placeholder.com/800x600?text=Sample+Image+5">
                                         </div>
                                         <div class="carousel-item h-100">
-                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://i.imgur.com/cglAhJI.jpg">
+                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://via.placeholder.com/800x600?text=Sample+Image+6">
                                         </div>
                                         <div class="carousel-item h-100">
-                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://i.imgur.com/C1TiHma.jpg">
+                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://via.placeholder.com/800x600?text=Sample+Image+7">
                                         </div>
                                         <div class="carousel-item h-100">
-                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://i.imgur.com/nOCf2Nb.jpg">
+                                            <img class="d-block mw-100 mh-100 mx-auto" src="https://via.placeholder.com/800x600?text=Sample+Image+8">
                                         </div>
                                     </div>
                                     <a class="carousel-control-prev" href="#carousel-gallery" role="button" data-slide="prev">
@@ -140,28 +95,28 @@
                     <div class="col-md-4 d-none d-md-block">
                         <ol class="carousel-indicators">
                             <li data-target="#carousel-gallery" data-slide-to="0" class="active">
-                                <div style="background-image: url(https://i.imgur.com/6JhjERc.jpg);"></div>
+                                <div style="background-image: url(https://via.placeholder.com/800x600?text=Sample+Image+1);"></div>
                             </li>
                             <li data-target="#carousel-gallery" data-slide-to="1">
-                                <div style="background-image: url(https://i.imgur.com/vsMPB7M.jpg);"></div>
+                                <div style="background-image: url(https://via.placeholder.com/800x600?text=Sample+Image+2);"></div>
                             </li>
                             <li data-target="#carousel-gallery" data-slide-to="2">
-                                <div style="background-image: url(https://i.imgur.com/9Fs94na.jpg);"></div>
+                                <div style="background-image: url(https://via.placeholder.com/800x600?text=Sample+Image+3);"></div>
                             </li>
                             <li data-target="#carousel-gallery" data-slide-to="3">
-                                <div style="background-image: url(https://i.imgur.com/3G0TYJo.jpg);"></div>
+                                <div style="background-image: url(https://via.placeholder.com/800x600?text=Sample+Image+4);"></div>
                             </li>
                             <li data-target="#carousel-gallery" data-slide-to="4">
-                                <div style="background-image: url(https://i.imgur.com/k8ycfKB.jpg);"></div>
+                                <div style="background-image: url(https://via.placeholder.com/800x600?text=Sample+Image+5);"></div>
                             </li>
                             <li data-target="#carousel-gallery" data-slide-to="5">
-                                <div style="background-image: url(https://i.imgur.com/cglAhJI.jpg); background-position-y: 20%;"></div>
+                                <div style="background-image: url(https://via.placeholder.com/800x600?text=Sample+Image+6);"></div>
                             </li>
                             <li data-target="#carousel-gallery" data-slide-to="6">
-                                <div style="background-image: url(https://i.imgur.com/C1TiHma.jpg);"></div>
+                                <div style="background-image: url(https://via.placeholder.com/800x600?text=Sample+Image+7);"></div>
                             </li>
                             <li data-target="#carousel-gallery" data-slide-to="7">
-                                <div style="background-image: url(https://i.imgur.com/nOCf2Nb.jpg);"></div>
+                                <div style="background-image: url(https://via.placeholder.com/800x600?text=Sample+Image+8);"></div>
                             </li>
                         </ol>
                     </div>
